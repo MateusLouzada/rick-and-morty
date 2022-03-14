9318d0686f2c066825b6423775749a9679e505c6
Mateus Louzada <mateuslouzada01@gmail.com>
2022-03-14 13:49:18 -0300

    Atualizado o ghpages

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,28 @@ import Episodes from './routes/Episodes'
 import Locations from './routes/Locations'
 import './App.css';
 import { Button } from '@mui/material';
+import logo from '../assets/img/logo.png'
 
 
 export default function App() {
 
   return (
     <div>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <div className='nav'>
           <div className='image'>
-            <img src="../assets/img/logo.png" alt="Logo rick and morty" />
+            <img src={logo} alt="Logo rick and morty" />
           </div>
           <ul className='nav-inside'>
-            <li><Link to="/"><Button>Characters</Button></Link></li>
-            <li><Link to="episodes"><Button>Episodes</Button></Link></li>
-            <li><Link to="locations"><Button>Locations</Button></Link></li>
+            <li><Link to="/rick-and-morty/"><Button>Characters</Button></Link></li>
+            <li><Link to="/rick-and-morty/episodes"><Button>Episodes</Button></Link></li>
+            <li><Link to="/rick-and-morty/locations"><Button>Locations</Button></Link></li>
           </ul>
         </div>
         <Routes>
-          <Route path="/" element={<Characters />} />
-          <Route path="episodes" element={<Episodes />} />
-          <Route path="locations" element={<Locations />} />
+          <Route path="/rick-and-morty/" element={<Characters />} />
+          <Route path="/rick-and-morty/episodes" element={<Episodes />} />
+          <Route path="/rick-and-morty/locations" element={<Locations />} />
         </Routes>
       </Router>
     </div>
